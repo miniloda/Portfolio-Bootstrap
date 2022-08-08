6308c538c51bbb8cfac1d3385bb344200a5ac2fd
Jacob maximus Usaraga <jlusaraga@up.edu.ph>
2022-08-08 12:26:44 +0800

    Revert "Revert "added a lot of features""
    This reverts commit 3eee9394d4caee977db3a9620d1e60c6660070da.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,65 @@
-document.onscroll = function() {
-    document.querySelector(".box").style.display = "none";
-}
 
 setInterval(()=>{
     if(window.scrollY === 0 ){
         document.querySelector(".box").style.display = "block";
+        $('.go-top').fadeOut('slow');
     }
 }, 1000);
 setInterval(()=>{
+    if(window.scrollY !== 0 ){
+        $('.box').fadeOut('slow');
+    }
+})
 
-}, 1000);
+$(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() +50 >= $(document).height()) {
+    $('.go-top').fadeIn('slow');
+
+   }
+});
+$(function(){
+
+  //Click event
+  $('.go-top').click(function () {
+    $("html, body").animate({ scrollTop: "0" },  1000);
+
+  });
+
+});
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
